d3f3059e845586004f15ceb7307cebda56876f98
lucuynswift <lucy.n.swift@gmail.com>
2026-07-11 01:33:16 +0800

    god bless me

--- app.py
+++ app.py
@@ -1589,6 +1589,9 @@ with tab1:
         save_progress()
 
     enter_key = (book_name, display_sentence)
+    # 确保状态已经被初始化
+    if "_sentence_enter_time" not in st.session_state:
+        st.session_state["_sentence_enter_time"] = {}
     if enter_key not in st.session_state["_sentence_enter_time"]:
         st.session_state["_sentence_enter_time"][enter_key] = time.time()
 
